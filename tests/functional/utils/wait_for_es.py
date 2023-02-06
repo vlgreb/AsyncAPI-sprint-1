abed4390730f95caa7f83556945f30091df86274
Igor <78171614+montekrist0@users.noreply.github.com>
2023-02-06 20:28:28 +0300

    add backoff for test waiters (#120)

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,10 +1,24 @@
 import logging
 import os
-import time
 
-from elasticsearch import Elasticsearch
+import backoff
+from elasticsearch import Elasticsearch, ElasticsearchException
 
-if __name__ == '__main__':
+
+def elastic_conn_backoff_hdlr(details):
+    logging.warning(
+        "\t\n ==> Elastic connection Error. "
+        "Backing off {wait:0.1f} seconds after {tries} tries "
+        "Details: {args}".format(**details))
+
+
+@backoff.on_exception(
+    wait_gen=backoff.expo,
+    exception=ElasticsearchException,
+    on_backoff=elastic_conn_backoff_hdlr,
+    max_tries=10
+)
+def check_elastic_connection():
 
     es_host = os.getenv('ELASTIC_HOST', default='localhost')
     es_port = os.getenv('ELASTIC_PORT', default=6379)
@@ -13,12 +27,14 @@ if __name__ == '__main__':
     es_client = Elasticsearch(
         hosts=[f'{es_host}:{es_port}'])
 
-    while True:
-        try:
-            if es_client.ping():
-                es_client.indices.refresh()
-                if int(es_client.cat.count(format="json")[0]['count']) >= es_total_count:
-                    break
-            time.sleep(1)
-        except Exception as exc:
-            logging.error('elastic is not ready\n\t%s', exc)
+    if es_client.ping():
+
+        if int(es_client.cat.count(format="json")[0]['count']) < es_total_count:
+            raise ElasticsearchException("Not enough data for testing. Wait for ETL finish loading...")
+
+    else:
+        raise ElasticsearchException("Elastic server is not available")
+
+
+if __name__ == '__main__':
+    check_elastic_connection()
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,21 +1,28 @@
+import logging
 import os
-import time
 
+import backoff
 from redis import Redis, exceptions
 
-if __name__ == '__main__':
 
+def redis_conn_backoff_hdlr(details):
+    logging.info(
+     "\t\n ==> Redis connection Error. "
+     "Backing off {wait:0.1f} seconds after {tries} tries "
+     "Details: {args}".format(**details))
+
+
+@backoff.on_exception(
+    wait_gen=backoff.expo,
+    exception=exceptions.ConnectionError,
+    on_backoff=redis_conn_backoff_hdlr,
+    max_tries=10
+)
+def check_redis_connection():
     redis_host = os.getenv('REDIS_HOST', default='localhost')
-
     redis = Redis(host=redis_host)
+    redis.ping()
 
-    while True:
 
-        try:
-            redis.ping()
-
-        except exceptions.ConnectionError:
-            time.sleep(1)
-
-        else:
-            break
+if __name__ == '__main__':
+    check_redis_connection()
